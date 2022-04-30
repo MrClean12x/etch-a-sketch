39045e6d5d41ddeade4cbe20dd590d03944200b7
Ryan <jesusc1289@gmail.com>
2022-04-29 21:29:20 -0400

    made grid creation into function, with potential to increase size

--- eas.js
+++ eas.js
@@ -1,10 +1,11 @@
-let grid = document.querySelector(".grid");
-grid.style.gridTemplateColumns = "repeat(16, 1fr)";
-grid.style.gridTemplateRows = "repeat(16, 1fr)";
+function createGrid(size) {
+    let grid = document.querySelector(".grid");
+    grid.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
-for (let i = 0; i < 256; i++) {
-    let square = document.createElement("div");
-    square.style.backgroundColor = "black";
-    grid.insertAdjacentElement("beforeend", square);
-
+    for (let i = 0; i < 256; i++) {
+        let square = document.createElement("div");
+        square.style.backgroundColor = "black";
+        grid.insertAdjacentElement("beforeend", square);
+    }
 }
