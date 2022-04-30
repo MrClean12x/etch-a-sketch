20c6d825f7df16c794c38dd278fc63033ed17160
Ryan <jesusc1289@gmail.com>
2022-04-29 21:52:09 -0400

    added the ability to change grid size

--- eas.js
+++ eas.js
@@ -16,6 +16,14 @@ function createGrid(size) {
 
 createGrid(16);
 
+function changeSize(input) {
+    if (input >= 2 && input <= 100){
+        createGrid(input);
+    } else {
+        console.log("too small or too large, between 2 and 100 only")
+    }
+}
+
 
 function colorSquare(){
     this.style.backgroundColor = "black";
